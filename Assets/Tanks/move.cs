d6d5e6a47bc541b315fc639cbf83118448b67355
malken21 <Malken_Marumasa@outlook.jp>
2023-04-07 22:16:01 +0900

    update

---
+++ Assets/Tanks/move.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+ 
+public class move : MonoBehaviour
+{
+    Rigidbody rb;
+    float speed = 3.0f;
+ 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+ 
+    void FixedUpdate()
+    {
+        // Wキー（前方移動）
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = transform.forward * speed;
+        }
+ 
+        // Sキー（後方移動）
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = - transform.forward * speed;
+        }
+ 
+        // Dキー（右移動）
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = transform.right * speed;
+        }
+ 
+        // Aキー（左移動）
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = -transform.right * speed;
+        }
+    }
+}
